bea084251b831041220b14a36e81ae37aa9d11c4
William Grant <williamg3575467@gmail.com>
2024-11-13 09:19:57 +0000

    Create server model

---
+++ db/migrate/20241113085103_create_servers.rb
@@ -0,0 +1,9 @@
+class CreateServers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :servers do |t|
+      t.string :name, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
