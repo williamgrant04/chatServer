2d225b3040ebeb87e2df56b9a529c9ddf9b7a64d
William Grant <williamg3575467@gmail.com>
2024-11-13 13:39:58 +0000

    See previous...

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,5 +1,13 @@
 class Server < ApplicationRecord
   belongs_to :user
   has_many :server_users
-  has_many :users, through: :server_users
+  has_many :members, through: :server_users, source: :user
+
+  after_create :create_server_user # Adds the server creator to the server as a member
+
+  protected
+
+  def create_server_user
+    ServerUser.create(server: self, user: self.user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :servers
+
+  # Allows referencing "user.servers" to pull all servers the user is a member of, not just servers the user owns
   has_many :server_users
+  has_many :servers, through: :server_users
 end
