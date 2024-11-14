6e3efee6bd7a21c31ad27738edd827f723735114
William Grant <williamg3575467@gmail.com>
2024-11-14 14:26:39 +0000

    Add basic validation to server model, because I forgot to before

--- app/models/server.rb
+++ app/models/server.rb
@@ -4,6 +4,9 @@ class Server < ApplicationRecord
   has_many :members, through: :server_users, source: :user
   has_many :channels, dependent: :destroy
 
+  validates :name, presence: true
+  validates :name, length: { minimum: 3, maximum: 50 }
+
   after_create :create_server_user # Adds the server creator to the server as a member
   after_create :create_default_channel
 
