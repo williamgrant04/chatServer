853864b796b2fdba6d6cc63ca8f28c3ce8d6d7ad
William Grant <williamg3575467@gmail.com>
2024-11-14 15:07:01 +0000

    Create pundit policy for servers only allowing server owner to edit details of a server.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  # before_action :authenticate_user!
+  include Pundit::Authorization
+
+  rescue_from Pundit::NotAuthorizedError, with: :not_authorized
+  def not_authorized
+    render json: { error: "You are not authorized to perform this action." }, status: 403
+  end
 end
--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,7 +1,6 @@
 class ServersController < ApplicationController
   def index # Will need a list of servers related to a user
-    @servers = Server.all
-    # current_user.servers will be what is used when it's implemented properly. But for now I'm just testing through postman.
+    @servers = current_user.servers
     render json: { servers: @servers }, status: 200
   end
 
@@ -13,6 +12,7 @@ class ServersController < ApplicationController
 
   def create
     @server = Server.new(server_params)
+    @server.user = current_user
 
     if @server.valid? && @server.save
       render json: { server: @server }, status: 201
@@ -23,6 +23,7 @@ class ServersController < ApplicationController
 
   def edit
     @server = Server.find(params[:id])
+    authorize @server
     @server.update(server_params)
 
     if @server.valid? && @server.save
