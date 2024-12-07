aa28be8b5776c16e802a81c45ac72544407cee2f
William Grant <williamg3575467@gmail.com>
2024-12-07 08:21:20 +0000

    Add validation for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :channel
   belongs_to :author, class_name: "User", foreign_key: "user_id" # Just to make easier to read later, I'm naming this "author" instead of "user"
+
+  validates :content, presence: true, length: { maximum: 1000 }, allow_blank: false
 end
